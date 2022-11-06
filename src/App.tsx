eaad76390a4873020b979e11d940a35291bd864e
iduk2 <iduk2@cj.net>
2022-11-06 18:59:06 +0900

    feat: Lazyload

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
 import * as React from 'react'
 import { useRoutes } from 'react-router-dom'
+import Loading from '@/components/Loading'
 
 // components
 import BaseLayout from '@/components/Layouts/BaseLayout'
 import Error404 from '@/components/Error/Error404'
-import Home from '@/pages/Home'
 
-function App(): JSX.Element {
+// Lazy loading
+const Home = React.lazy(() => import('@/pages/Home'))
+const Login = React.lazy(() => import('@/pages/Login/Login'))
+const Page = React.lazy(() => import('./pages/Page/Page'))
+
+export default function App(): JSX.Element {
   const routes = {
     path: '/',
     element: <BaseLayout />,
+    layout: 'BaseLayout',
     children: [
       {
         path: '*',
         element: <Error404 />,
-        title: 'error'
+        title: 'Error'
       },
       {
         path: '/',
         element: <Home />,
-        title: 'home'
+        title: 'Home'
+      },
+      {
+        path: '/login',
+        element: <Login />,
+        title: 'Login'
+      },
+      {
+        path: '/page',
+        element: <Page />,
+        title: 'Page'
       }
     ]
   }
 
   const routing = useRoutes([routes])
 
-  return <>{routing}</>
+  return <React.Suspense fallback={<Loading />}>{routing}</React.Suspense>
 }
-export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Loading from '@/components/Loading'
 import styled from '@emotion/styled'
 import styles from './Home.module.scss'
 import classnames from 'classnames/bind'
@@ -10,115 +9,111 @@ import dataJson from './chart.json'
 import ReactChart from './ReactChart'
 
 export default function Home(): JSX.Element {
-  const [loading, setLoading] = useState(true)
+  // const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 500)
-  }, [])
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 500)
+  // }, [])
 
   return (
     <>
-      {loading === false ? (
-        <div className="container">
-          <ThemeWrap>
-            <h6>Theme Mode</h6>
-            <section className="p-4 mt-lg-3 bg-gray-3">
-              <div className="row">
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
-                  1
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
-                  2
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
-                  3
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
-                  4
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
-                  5
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
-                  6
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
-                  7
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
-                  8
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
-                  9
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
-                  10
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
-                  11
-                </span>
-                <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
-                  12
-                </span>
-              </div>
-            </section>
-
-            <section className="p-4 mt-3">
-              <div className="row">
-                <span className="col-lg-3 col-md-6 col-6 bg-gray-3">1</span>
-                <span className="col-lg-3 col-md-6 col-6 bg-gray-4">2</span>
-                <span className="col-lg-3 col-md-6 col-6 bg-gray-5">3</span>
-                <span className="col-lg-3 col-md-6 col-6 bg-gray-6">4</span>
-              </div>
-            </section>
-          </ThemeWrap>
-
-          <div>
-            <h3 className={cx('title')}>ChartJS</h3>
+      <div className="container">
+        <ThemeWrap>
+          <h6>Theme Mode</h6>
+          <section className="p-4 mt-lg-3 bg-gray-3">
             <div className="row">
-              <div className="col-lg-6 col-12">
-                <section className={cx('card')}>
-                  <div className="p-3">
-                    <small>Total Payments</small>
-                    <h4 className="my-1">8,900</h4>
-                  </div>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
+                1
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
+                2
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
+                3
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
+                4
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
+                5
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
+                6
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
+                7
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
+                8
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
+                9
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
+                10
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-6">
+                11
+              </span>
+              <span className="col-lg-1 col-md-3 col-sm-6 col-12 bg-gray-7">
+                12
+              </span>
+            </div>
+          </section>
 
-                  <div style={{ height: 200 }}>
-                    <ReactChart
-                      type="line"
-                      options={options}
-                      data={LineChartData}
-                    />
-                  </div>
-                </section>
-              </div>
-              <div className="col-lg-6 col-12">
-                <section className={cx('card')}>
-                  <div className="p-3">
-                    <small>Total Payments</small>
-                    <h4 className="my-1">192,345</h4>
-                  </div>
+          <section className="p-4 mt-3">
+            <div className="row">
+              <span className="col-lg-3 col-md-6 col-6 bg-gray-3">1</span>
+              <span className="col-lg-3 col-md-6 col-6 bg-gray-4">2</span>
+              <span className="col-lg-3 col-md-6 col-6 bg-gray-5">3</span>
+              <span className="col-lg-3 col-md-6 col-6 bg-gray-6">4</span>
+            </div>
+          </section>
+        </ThemeWrap>
 
-                  <div style={{ height: 200 }}>
-                    <ReactChart
-                      type="bar"
-                      options={options}
-                      data={BarChartData}
-                    />
-                  </div>
-                </section>
-              </div>
+        <div>
+          <h3 className={cx('title')}>ChartJS</h3>
+          <div className="row">
+            <div className="col-lg-6 col-12">
+              <section className={cx('card')}>
+                <div className="p-3">
+                  <small>Total Payments</small>
+                  <h4 className="my-1">8,900</h4>
+                </div>
+
+                <div style={{ height: 200 }}>
+                  <ReactChart
+                    type="line"
+                    options={options}
+                    data={LineChartData}
+                  />
+                </div>
+              </section>
+            </div>
+            <div className="col-lg-6 col-12">
+              <section className={cx('card')}>
+                <div className="p-3">
+                  <small>Total Payments</small>
+                  <h4 className="my-1">192,345</h4>
+                </div>
+
+                <div style={{ height: 200 }}>
+                  <ReactChart
+                    type="bar"
+                    options={options}
+                    data={BarChartData}
+                  />
+                </div>
+              </section>
             </div>
           </div>
-
-          <div>
-            <h3 className={cx('title')}>mui DataGrid</h3>
-            <DatagridTest />
-          </div>
         </div>
-      ) : (
-        <Loading />
-      )}
+
+        <div>
+          <h3 className={cx('title')}>mui DataGrid</h3>
+          <DatagridTest />
+        </div>
+      </div>
     </>
   )
 }
